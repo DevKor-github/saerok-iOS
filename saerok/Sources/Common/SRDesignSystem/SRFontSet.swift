5888c31a5efea8525651c1edb55d9cdb2405dfa6
Hanseung Cho <1win2@naver.com>
2025-04-08 22:34:08 +0900

    fix: 폰트 사이즈 및 Info.plist 수정

--- saerok/Sources/Common/SRDesignSystem/SRFontSet.swift
+++ saerok/Sources/Common/SRDesignSystem/SRFontSet.swift
@@ -11,7 +11,8 @@ extension Font {
     enum SRFontSet {
         static let h1: Font = .custom(Pretendard.semiBold.rawValue, size: 24.0)
         static let h2: Font = .custom(Pretendard.semiBold.rawValue, size: 20.0)
-        static let h3: Font = .custom(Pretendard.regular.rawValue, size: 20.0)
+        static let h3: Font = .custom(Pretendard.medium.rawValue, size: 15.0)
+        static let h4: Font = .custom(Pretendard.regular.rawValue, size: 13.0)
 
         static let tabbar: Font = .custom(Pretendard.regular.rawValue, size: 11).weight(.regular)
         static let tabbarSelected: Font = .custom(Pretendard.medium.rawValue, size: 11).bold()
