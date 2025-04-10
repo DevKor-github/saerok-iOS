649208c9321b779a0ea10481bd9dc3548ea7518c
Hanseung Cho <1win2@naver.com>
2025-04-10 22:22:20 +0900

    feat: 라우팅이 가능한 뷰 프로토콜 작성

--- saerok/Sources/Common/Utils/Routable.swift
+++ saerok/Sources/Common/Utils/Routable.swift
@@ -1,11 +1,22 @@
-protocol BaseView: View {
+//
+//  Routable.swift
+//  saerok
+//
+//  Created by HanSeung on 4/9/25.
+//
+
+
+import Combine
+import SwiftUI
+
+protocol Routable: View {
     associatedtype Routing: Equatable
     var routingState: Routing { get set }
     var routingBinding: Binding<Routing> { get }
     var routingUpdate: AnyPublisher<Routing, Never> { get }
 }
 
-extension BaseView {
+extension Routable {
     var routingBinding: Binding<Routing> {
         fatalError("Must override routingBinding in your View")
     }
